5d9cbe774040f5ffdebc9cff14f7fdf2075a0046
8848Mateusz <8848mateusz@gmail.com>
2024-02-20 20:23:25 +0100

    stworzenie dbutil

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,21 +1,8 @@
 package pl.coderslab;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.*;
+
 
 public class TaskManager {
-    public class DbUtil {
-        private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
-        private static final String DB_USER = "root";
-        private static final String DB_PASS = "coderslab";
-
-        public static Connection getConnection() throws SQLException {
-            System.out.println("jest git");
-            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-        }
-    }
 
 
 }
